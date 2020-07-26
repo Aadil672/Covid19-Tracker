8d7c72c799b4afc60776d42ac5078c1cf8ea9d2e
Aadil Hussain <aadil.bit007@gmail.com>
2020-07-27 04:37:46 +0530

    Add files via upload
    first commit with build files of COVID 19 tracker

---
+++ build/precache-manifest.d880a50751e41fd783bbe9f08fdd3cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d48250a99299bb53fd0657c7213932b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46a44b7cd2f6cff2893",
+    "url": "/static/css/main.b387d687.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46a44b7cd2f6cff2893",
+    "url": "/static/js/main.3cb80a7d.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
